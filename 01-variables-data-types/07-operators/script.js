3ac0fc88abd4559912fd9afe60f15e67c427dbd1
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-06-26 00:43:09 +0530

    17

--- 01-variables-data-types/07-operators/script.js
+++ 01-variables-data-types/07-operators/script.js
@@ -1,7 +1,51 @@
 // 1. Arithmetic Operators
 
+let x;
 
-// 2. Assignment Operators
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;
 
+// Concatenation
+x = 'Hello' + ' ' + 'World';
+
+// Exponent
+
+x = 2 ** 3;
+
+// Increment
+x = 1;
+x = x + 1;
+x++;
+
+// Decrement
+// x = x -1
+x--;
+
+// Assignment Operators
+
+x = 10;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+x **= 5;
 
 // 3. Comparison Operators
+
+x = 2 === '2';
+
+x = 2 === '2';
+
+x = 2 != '2';
+
+x = 2 !== '2';
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x);
