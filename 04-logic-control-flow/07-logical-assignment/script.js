9f7621b7c1fba4ebbd996bbd1789c87fcdca7d6b
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-06-28 18:13:52 +0530

    63

---
+++ 04-logic-control-flow/07-logical-assignment/script.js
@@ -0,0 +1,30 @@
+let a = 20;
+
+if (!a) {
+  a = 10;
+}
+
+a = a || 10;
+
+a ||= 10;
+
+let b = 10;
+
+if (b) {
+  b = 20;
+}
+
+b = b && 20;
+b &&= 20;
+
+let c = null;
+
+if (c === null || c === undefined) {
+  c = 20;
+}
+
+c = c ?? 20;
+
+c ??= 20;
+
+console.log(c);
