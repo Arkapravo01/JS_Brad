2e74096592fce67198e869cd9b762979ef5b4e5d
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-06-29 22:15:09 +0530

    70

---
+++ 06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,15 @@
+// console.log(document.body.innerHTML)
+// console.log(window.document)
+
+// console.log(document.links[0]);
+
+// document.body.innerHTML = '<h1>Hello World</h1>'
+
+// document.write('Hello from JS');
+
+console.log(document.getElementById('main'));
+const main = document.getElementById('main');
+
+// main.innerHTML = '<h1>Hello from main</h1>';
+
+document.querySelector('#main h1').innerText = 'Hello';
