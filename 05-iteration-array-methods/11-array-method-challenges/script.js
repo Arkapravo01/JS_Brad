5a91f02cf8555717dc51b39ca67aa3ef52eeb82f
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-06-29 21:32:21 +0530

    67

--- 05-iteration-array-methods/11-array-method-challenges/script.js
+++ 05-iteration-array-methods/11-array-method-challenges/script.js
@@ -37,5 +37,31 @@ const people = [
   },
 ];
 
+const youngPeople = people
+  .filter((person) => person.age <= 25)
+  .map((person) => {
+    return {
+      name: `${person.firstName} ${person.lastName}`,
+      email: person.email,
+    };
+  });
+
+console.log(youngPeople);
+
 // Challenge 2
 const numbers = [2, -30, 50, 20, -12, -9, 7];
+
+const positiveSum = numbers
+  .filter((num) => num > 0)
+  .reduce((acc, curr) => acc + curr, 0);
+
+console.log(positiveSum);
+
+// Challenge 3
+const words = ['coder', 'programmer', 'developer'];
+
+const capitalizedWords = words.map(
+  (word) => `${word[0].toUpperCase()}${word.slice(1)}`,
+);
+
+console.log(capitalizedWords);
