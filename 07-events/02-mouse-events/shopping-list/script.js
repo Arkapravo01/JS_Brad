d99dccbc611ff44d0c8d1b5c4724e523b303b86f
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-07-02 20:05:26 +0530

    87

--- 07-events/02-mouse-events/shopping-list/script.js
+++ 07-events/02-mouse-events/shopping-list/script.js
@@ -1,1 +1,34 @@
+const logo = document.querySelector('img');
+
+const onClick = () => console.log('click event');
+const onDoubleClick = () => {
+  if (document.body.style.backgroundColor !== 'purple') {
+    document.body.style.backgroundColor = 'purple';
+    document.body.style.color = 'white';
+  } else {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+  }
+};
+const onRightClick = () => console.log('right click event');
+const onMouseDown = () => console.log('on mouse down');
+const onMouseUp = () => console.log('on mouse up');
+const onMouseWheel = () => console.log('on mouse wheel');
+const onMouseOver = () => console.log('on mouse over');
+const onMouseOut = () => console.log('on mouse out');
+const onDragStart = () => console.log('drag start event');
+const onDrag = () => console.log('drag event');
+const onDragEnd = () => console.log('drag end event');
+
+// Event Listeners
+logo.addEventListener('click', onClick);
+logo.addEventListener('dblclick', onDoubleClick);
+logo.addEventListener('contextmenu', onRightClick);
+logo.addEventListener('mousedown', onMouseDown);
+logo.addEventListener('mouseup', onMouseUp);
+logo.addEventListener('wheel', onMouseWheel);
+logo.addEventListener('mouseover', onMouseOver);
+logo.addEventListener('dragstart', onDragStart);
+logo.addEventListener('drag', onDrag);
+logo.addEventListener('dragend', onDragEnd);
 
