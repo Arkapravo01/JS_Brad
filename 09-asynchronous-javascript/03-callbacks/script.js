0dcd4e1a13807139a486458df926dbfaa57f360c
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-07-05 17:17:43 +0530

    122

--- 09-asynchronous-javascript/03-callbacks/script.js
+++ 09-asynchronous-javascript/03-callbacks/script.js
@@ -10,11 +10,19 @@ const posts = [
   { title: 'Post Two', body: 'This is post two' },
 ];
 
-function createPost(post, cb) {
-  setTimeout(() => {
-    posts.push(post);
-    cb();
-  }, 2000);
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let error = false;
+
+      if (!error) {
+        posts.push(post);
+        resolve();
+      } else {
+        reject('Something went wrong');
+      }
+    }, 2000);
+  });
 }
 
 function getPosts() {
@@ -27,6 +35,14 @@ function getPosts() {
   }, 1000);
 }
 
-createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
+function showError(error) {
+  const h3 = document.createElement('h3');
+  h3.innerHTML = `<strong>${error}</strong>`;
+  document.getElementById('posts').appendChild(h3);
+}
+
+createPost({ title: 'Post Three', body: 'This is post three' })
+  .then(getPosts)
+  .catch(showError);
 
 // getPosts();
--- 09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
+++ 09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
@@ -1,25 +1,56 @@
-function getData(endpoint, cb) {
-  const xhr = new XMLHttpRequest();
+function getData(endpoint) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', endpoint);
+    xhr.open('GET', endpoint);
 
-  xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      cb(JSON.parse(this.responseText));
-    }
-  };
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          resolve(JSON.parse(this.responseText));
+        } else {
+          reject('Something went wrong');
+        }
+      }
+    };
 
-  setTimeout(() => {
-    xhr.send();
-  }, Math.floor(Math.random() * 3000) + 1000);
+    setTimeout(
+      () => {
+        xhr.send();
+      },
+      Math.floor(Math.random() * 3000) + 1000,
+    );
+  });
 }
 
-getData('./movies.json', (data) => {
-  console.log(data);
-  getData('./actors.json', (data) => {
-    console.log(data);
-    getData('./directors.json', (data) => {
-      console.log(data);
-    });
-  });
+// getData('./movies.json')
+//   .then((movies) => {
+//     console.log(movies);
+//     return getData('./actors.json');
+//   })
+//   .then((actors) => {
+//     console.log(actors);
+//     return getData('./directors.json');
+//   })
+//   .then((directors) => {
+//     console.log(directors);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
+const moviesPromise = getData('./movies.json');
+const actorsPromise = getData('./actors.json');
+const directorsPromise = getData('./directors.json');
+
+const dummyPromise = new Promise((resolve, reject) => {
+  resolve('Hello World');
 });
+
+Promise.all([moviesPromise, actorsPromise, directorsPromise,dummyPromise])
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
